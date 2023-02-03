fbe5d62c4bf23a1c191e2ddeddacd845c6ee4962
jbpcasoy <johnbryanpitcasoy@gmail.com>
2023-02-03 21:32:31 +0800

    implemented view chirps

--- resources/js/Pages/Chirps/Index.jsx
+++ resources/js/Pages/Chirps/Index.jsx
@@ -1,42 +1,48 @@
-import { Head, useForm } from '@inertiajs/react'
-import React from 'react'
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout"
-import InputError from '@/Components/InputError';
-import PrimaryButton from '@/Components/PrimaryButton';
-import Chirp from '@/Components/Chirp';
+import Chirp from "@/Components/Chirp";
+import InputError from "@/Components/InputError";
+import PrimaryButton from "@/Components/PrimaryButton";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head, useForm } from "@inertiajs/react";
 
-export default function Index({auth, chirps}) {
-    const {data, setData, post, processing, reset, errors, } = useForm(
-        {
-            message: '',
-        }
-    );
+export default function Index({ auth, chirps }) {
+    const { data, setData, post, processing, reset, errors } = useForm({
+        message: "",
+    });
 
-
-    const submit = e => {
-        post(route("chirps.store", {
-            onSuccess: () => {
-                reset();
-            }
-        }))
+    const submit = (e) => {
+        post(
+            route("chirps.store", {
+                onSuccess: () => {
+                    reset();
+                },
+            })
+        );
     };
 
     return (
         <AuthenticatedLayout auth={auth}>
-            <Head title='Chirps'/>
+            <Head title="Chirps" />
             <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
                 <form onSubmit={submit}>
-                    <textarea className="w-full border-gray-300 focus:border-indigo-300 rounded-md  focus:ring focus:ring-indigo-200 focus:ring-opacity-50 shadow-sm" placeholder="What's in your mind?" value={data.message} onChange={e => setData({...data, message: e.target.value})}></textarea>
-                    <InputError message={errors.message} className = "mt-2"/>
-                    <PrimaryButton className='mt-4' processing={processing}>Chirp</PrimaryButton>
+                    <textarea
+                        className="w-full border-gray-300 focus:border-indigo-300 rounded-md  focus:ring focus:ring-indigo-200 focus:ring-opacity-50 shadow-sm"
+                        placeholder="What's in your mind?"
+                        value={data.message}
+                        onChange={(e) =>
+                            setData({ ...data, message: e.target.value })
+                        }
+                    ></textarea>
+                    <InputError message={errors.message} className="mt-2" />
+                    <PrimaryButton className="mt-4" processing={processing}>
+                        Chirp
+                    </PrimaryButton>
                 </form>
+                <div className="mt-6 bg-white shadow-sm rounded-lg divide-y">
+                    {chirps?.map((chirp) => {
+                        return <Chirp chirp={chirp} />;
+                    })}
+                </div>
             </div>
-            <div>
-                {chirps?.map(chirp => {
-                    return <Chirp chirp={chirp}/>
-                })}
-            </div>
-
         </AuthenticatedLayout>
-    )
+    );
 }
